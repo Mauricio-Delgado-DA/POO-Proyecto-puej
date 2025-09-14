1cd3a96739db34706fdd9e39b5b94656878a5a84
Aldha <Aldha@LaptopAldhair>
2025-09-14 12:44:55 -0500

    JOAO ALDHAIR SALAZAR MANRIQUE

---
+++ mavenproject1/src/main/java/Clases/Usuario.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Clases;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Aldha
+ */
+public class Usuario {
+    private int IdUsuario;
+    private String Nombre;
+    private String Email;
+    private String Contraseña;
+    private List<String> Preferencias;
+
+    public boolean ValidarCorreo(String correo){
+        Pattern CorreoPatron=Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+        Matcher matcher = CorreoPatron.matcher(correo);
+        return matcher.matches();
+    } 
+    
+    public void Registro(){
+    }
+    public void Inciar_Sesion(){
+    }
+    public void Actualizar_Perfil(){
+    }
+
+}   
